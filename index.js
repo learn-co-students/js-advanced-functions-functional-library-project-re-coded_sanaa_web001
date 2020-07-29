ab2d5211031c8c2ff0e6251a3d7ebb08d192a672
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-29 23:22:00 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(array, alert) {
+      if (array.length){
+        array.forEach((x) => alert(x))
+      }else
+      {
+        for (const property in array) {
+                alert(array[property])
+         }
+      }
+       return array;
     },
 
-    map: function() {
+    map: function(array, callback) {
+      if (array.length){
+        return array.map(function (x) { return callback(x)} );
+      }else
+      {
+        let res = [];
+        for (const property in array) {
+           res.push(callback(array[property]));
+         }
+         return res;
+         }
 
     },
 
